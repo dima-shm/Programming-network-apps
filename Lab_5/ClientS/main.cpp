0ce12ca1dda82ba9a501670c6dce1f7a82f062c5
Dima Shumanski <dima-shumanski@yandex.ru>
2017-11-11 15:22:27 +0300

    Added Lab_5 — To determine address of computer by its symbolic name

---
+++ Lab_5/ClientS/main.cpp
@@ -0,0 +1,79 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include "func_ErrorMsgText.h"
+#include "Winsock2.h"
+
+WSADATA wsaData;
+SOCKET cS;
+SOCKADDR_IN serv;
+
+bool GetServerByName(char* name, char* call, struct sockaddr* from, int* flen)
+{
+	int countOfSendedBytes = 0;
+	int countOfReceivedBytes = 0;
+	char msgFromServer[50];
+	memset(&msgFromServer, 0, sizeof(msgFromServer));
+
+	hostent *servInfo;
+	servInfo = gethostbyname(name);
+
+	in_addr *addr_list;
+	addr_list = (in_addr*)*(servInfo->h_addr_list);
+
+	serv.sin_addr = *addr_list;
+
+	if ((countOfSendedBytes = sendto(cS, call, strlen(call), NULL, (sockaddr*)&serv, sizeof(serv))) == SOCKET_ERROR)
+		throw SetErrorMsgText("sendto: ", WSAGetLastError());
+
+	if ((countOfReceivedBytes = recvfrom(cS, msgFromServer, sizeof(msgFromServer), NULL, from, flen)) == SOCKET_ERROR)
+		throw SetErrorMsgText("recvfrom: ", WSAGetLastError());
+
+	cout << "Ответ от сервера: " << msgFromServer << endl << endl;
+
+	return (strcmp(name, msgFromServer) == 0);
+}
+
+void main()
+{
+	setlocale(0, "");
+
+	int sizeOfServ = sizeof(serv);
+	short servPort = 2000;
+	char* servName = "dima-shm";
+
+	serv.sin_family = AF_INET;	 // используется IP-адресация
+	serv.sin_port = htons(2000); // порт 2000
+
+	try 
+	{
+		if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+			throw SetErrorMsgText("WSAStartup: ", WSAGetLastError());
+
+		if ((cS = socket(AF_INET, SOCK_DGRAM, NULL)) == INVALID_SOCKET)
+			throw SetErrorMsgText("socket: ", WSAGetLastError());
+
+		int optval = 1;
+		if (setsockopt(cS, SOL_SOCKET, SO_BROADCAST, (char*)&optval, sizeof(int)) == SOCKET_ERROR)
+			throw SetErrorMsgText("setsockopt: ", WSAGetLastError());
+
+		if (GetServerByName("DIMA", servName, (sockaddr*)&serv, &sizeOfServ))
+		{
+			cout << "IP-адрес сервера: " << inet_ntoa(serv.sin_addr) << endl;
+			cout << "Порт:             " << serv.sin_port << endl;
+		}
+		else
+		{
+			cout << "Сервер с таким именем не найден" << endl;
+		}
+
+		if (closesocket(cS) == SOCKET_ERROR)
+			throw SetErrorMsgText("closesocket: ", WSAGetLastError());
+
+		if (WSACleanup() == SOCKET_ERROR)
+			throw SetErrorMsgText("WSACleanup: ", WSAGetLastError());
+	}
+	catch (string errorMessage)
+	{
+		cout << errorMessage << endl;
+	}
+	_getch();
+}
